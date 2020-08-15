56b07380cf023140d1284bee5f93f01022280dc0
zlincon <38511541+zlincon@users.noreply.github.com>
2020-08-15 00:38:18 -0300

    #

--- mobile/App.tsx
+++ mobile/App.tsx
@@ -1,24 +1,27 @@
 import { StatusBar } from 'expo-status-bar';
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
-import Landing from './src/pages/Landing';
+import { AppLoading } from 'expo';
+import { Archivo_400Regular, Archivo_700Bold, useFonts } from '@expo-google-fonts/archivo';
+import { Poppins_400Regular, Poppins_600SemiBold } from '@expo-google-fonts/poppins';
+import AppStack from './src/routes/AppStack';
 
 export default function App() {
-  return (
-    <Landing />
-  );
-}
+  let [ fontsLoaded ] = useFonts({
+    Archivo_400Regular,
+    Archivo_700Bold,
+    Poppins_400Regular,
+    Poppins_600SemiBold
+  });
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#333',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-  title: {
-    fontSize: 32,
-    color: '#fff',
-    fontWeight: 'bold',
+  if(!fontsLoaded) {
+    return <AppLoading />
+  } else {
+    return (
+      <>
+        <AppStack />
+        <StatusBar style="light" />
+      </>
+    );
   }
-});
+
+}
