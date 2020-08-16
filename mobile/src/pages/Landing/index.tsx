4d2ab3428a029b5ccfb0eea350331cd96e5e64cf
zlincon <38511541+zlincon@users.noreply.github.com>
2020-08-15 23:01:36 -0300

    Conclusão #NLW2

--- mobile/src/pages/Landing/index.tsx
+++ mobile/src/pages/Landing/index.tsx
@@ -1,20 +1,34 @@
-import React from 'react';
-import { View, Image, Text, TouchableOpacity } from 'react-native';
+import React, { useState, useEffect } from 'react';
+import { View, Image, Text } from 'react-native';
 import { useNavigation } from '@react-navigation/native'
 import { RectButton } from 'react-native-gesture-handler';
-
-import styles from './styles';
+import api from '../../services/api';
 
 import landingImg from '../../assets/images/landing.png';
 import studyIcon from '../../assets/images/icons/study.png';
 import giveClassesIcons from '../../assets/images/icons/give-classes.png';
 import heartIcon from '../../assets/images/icons/heart.png';
 
+import styles from './styles';
+
 function Landing() {
     const {navigate} = useNavigation();
+    const [totalConnetions, setTotalConnections] = useState(0);
+
+    useEffect(() => {
+        api.get('connections').then(response => {
+            const { total } = response.data;
+
+            setTotalConnections(total);
+        })
+    }, []);
 
     function handleNavigateToGivePage() {
         navigate('GiveClasses');
+    }
+
+    function handleNavigateToStudyPages() {
+        navigate('Study');
     }
 
     return (
@@ -27,7 +41,10 @@ function Landing() {
             </Text>
 
             <View style={styles.buttonsContainer}>
-                <RectButton style={[styles.button, styles.buttonPrimary]}>
+                <RectButton 
+                    onPress={handleNavigateToStudyPages}
+                    style={[styles.button, styles.buttonPrimary]}
+                >
                     <Image source={studyIcon} />
                     <Text style={styles.buttonText}>Estudar</Text>
                 </RectButton>
@@ -42,7 +59,7 @@ function Landing() {
             </View>
 
             <Text style={styles.totalConnections}>
-                Total de 285 conexões já realidas {'    '}
+                Total de {totalConnetions} conexões já realidas {'    '}
                 <Image source={heartIcon} />
             </Text>
         </View>
